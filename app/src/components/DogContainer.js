7b3d234e32e56b413745916046790743974febbc
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-11-04 11:55:27 -0800

    added styling to my images

--- app/src/components/DogContainer.js
+++ app/src/components/DogContainer.js
@@ -1,61 +1,68 @@
-import React from 'react'
-import styled from 'styled-components'; 
-import { connect } from 'react-redux';
-import { fetchDogs, clearDogs } from './../actions/index';
-
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { fetchDogs, clearDogs } from "./../actions/index";
+import { AnimatePresence, motion } from "framer-motion";
 
 const StyledDogContainer = styled.div`
+  & {
+  }
 
-& {
- 
-}
+  .dog-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
 
-.dog-container {
-    display: flex; 
-    justify-content: center; 
-    align-items: center; 
-    flex-wrap: wrap; 
-}
+  img {
+    height: 300px;
+    width: 350px;
+    border: 2px solid white;
+    -moz-box-shadow: 10px 10px 50px black;
+    -webkit-box-shadow: 10px 10px 50px black;
+    box-shadow: 10px 10px 50px black;
+  }
 
 
-
-img {
-    height: 300px; 
-    width: 350px; 
-    margin: 1%; 
-}
-
-`
+  .dogs {
+      margin: 1%; 
+  }
+`;
 
 const DogContainer = (props) => {
+  console.log(props);
 
-    
-
-    console.log(props)
-
-    const renderedList = props.dogs.map((cur, index) => {
-        return (
-            <img src={cur} key={index}  /> 
-        )
-    })
-
+  const renderedList = props.dogs.map((cur, index) => {
     return (
-        <StyledDogContainer>
-            <button onClick={props.fetchDogs}>Add!</button>
-            <button onClick={props.clearDogs}>Clear List!</button>
-            <div className="dog-container">
-                {renderedList}
-            </div>
-        </StyledDogContainer>
-    )
-}
+      <AnimatePresence>
+        <motion.div
+          exit={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.5 }}
+          key={index}
+          className="dogs"
+        >
+          <img src={cur} />
+        </motion.div>
+      </AnimatePresence>
+    );
+  });
 
-const mapStateToProps = state => {
+  return (
+    <StyledDogContainer>
+      <button onClick={props.fetchDogs}>Add!</button>
+      <button onClick={props.clearDogs}>Clear List!</button>
+      <div className="dog-container">{renderedList}</div>
+    </StyledDogContainer>
+  );
+};
 
+const mapStateToProps = (state) => {
+  return {
+    dogs: state.dogs,
+  };
+};
 
-    return {
-        dogs: state.dogs, 
-    }
-}
-
-export default connect(mapStateToProps, { fetchDogs, clearDogs })(DogContainer)
+export default connect(mapStateToProps, { fetchDogs, clearDogs })(DogContainer);
